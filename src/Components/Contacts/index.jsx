0e66c021d43d728504b3437c79c7b3ceca6edf23
hazalnurasma <asmahazalnur@gmail.com.tr>
2024-03-09 13:57:41 +0300

    driver.js only shown to user after clicking info button

--- src/Components/Contacts/index.jsx
+++ src/Components/Contacts/index.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import List from './List';
 import Form from './Form';
 import "./contactIndex.css";
@@ -8,51 +8,59 @@ import "driver.js/dist/driver.css";
 
 function Contacts() {
     const [contacts, setContacts] = useState([]);
+    const [start, setStart] = useState(false); // Başlangıçta false
 
     useEffect(() => {
         console.log(contacts);
-    }, [contacts])
+    }, [contacts]);
 
     useEffect(() => {
+        if (start) {
+            const driverObj = driver({
+                showProgress: true,
+                steps: [
+                    {
+                        popover: {
+                            title: "Welcome!",
+                            description: "This is an app to add contacts and filter them!",
+                            side: "over",
+                        }
+                    },
+                    {
+                        element: ".list",
+                        popover: {
+                            title: "Filtered Contacts",
+                            description: "You can filter your contacts here",
+                            side: "top",
+                        }
+                    },
+                    {
+                        element: ".form",
+                        popover: {
+                            title: "Add Contact",
+                            description: "Add contacts from here!",
+                            side: "left",
+                        }
+                    },
+                ]
+            });
+            driverObj.drive();
+        }
+    }, [start]);
 
-        const driverObj = driver({
-          showProgress: true,
-          steps: [
-            {
-                popover: {
-                    title:"Welcome!",
-                    description: "This is an app to add contacts and filtered them!",
-                    side:"over",
-                }
-            },
-            {
-              element: ".list",
-              popover: {
-                title: " Filtered Contacts",
-                description: "You can filter your contacts here",
-                side: "top",
-              }
-            },
-            {
-              element: ".form",
-              popover: {
-                title: "Add Contact",
-                description: "Add contacts from here!",
-                side: "left",
-              }
-            },
-          ]
-        });
-        driverObj.drive();
-      });
+    // info butonuna basıldığında start state'ini true yap
+    const startTour = () => {
+        setStart(true);
+    }
 
     return (
         <div>
-            <BsInfoCircle className="info" />
-            <List className="list" contacts={contacts}/>
-            <Form className="form" addContact={setContacts} contacts={contacts}/>
+            {/* BsInfoCircle bileşenine onClick olayını startTour fonksiyonuyla bağlayın */}
+            <BsInfoCircle className="info" onClick={startTour} />
+            <List className="list" contacts={contacts} />
+            <Form className="form" addContact={setContacts} contacts={contacts} />
         </div>
-  )
+    )
 }
 
-export default Contacts
+export default Contacts;
